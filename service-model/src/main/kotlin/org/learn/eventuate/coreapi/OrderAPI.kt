5d86fb4635d42ad9757e7292991eac0a3f53fd97
xstefank <xstefank122@gmail.com>
2017-12-10 10:07:53 +0100

    clean up service-model - fix cdc discovery

--- service-model/src/main/kotlin/org/learn/eventuate/coreapi/OrderAPI.kt
+++ service-model/src/main/kotlin/org/learn/eventuate/coreapi/OrderAPI.kt
@@ -1,15 +1,12 @@
 package org.learn.eventuate.coreapi
 
-import io.eventuate.Event
-import io.eventuate.EventEntity
-
 
 //domain
 data class ProductInfo(val productId: String = "", val comment: String = "", val price: Int = 0)
 
+data class OrderSagaInfo(val sagaId: String = "", val productInfo: ProductInfo? = null)
 
-@EventEntity(entity = "org.learn.eventuate.orderservice.domain.OrderAggregate")
-interface OrderEvent : Event
+data class ShipmentInfo(val sagaId: String = "", val shipmentId: String = "", val shipmentPrice: Int = 0)
+data class InvoiceInfo(val sagaId: String = "", val invoiceId: String = "", val invoice: String = "")
 
-data class OrderFiledEvent(val orderId: String = "", val productInfo: ProductInfo? = null) : OrderEvent
-class OrderCompletedEvent(val orderId: String = "", val productInfo: ProductInfo? = null) : OrderEvent
+data class ParticipantFailureInfo(val sagaId: String = "", val id: String = "", val cause: String = "")
